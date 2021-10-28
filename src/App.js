f9a9c0182b1efdc488ccec4b59027b1467b5c77e
Nick <nickmagi24@gmail.com>
2021-10-28 17:39:23 -0400

    Add hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
+      <Header />
       <Hero />
-      <About />
+      {/* <About />
       <Cards />
       <Footer /> */}
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function Header() {
     return(
-        <div>
-
+        <div class="container">
+            <nav class="header">
+                <div class="logo">Header Logo</div>
+                <ul>
+                <li>header link one</li> 
+                <li>header link two</li> 
+                <li>header link three</li> 
+                </ul>
+            </nav>
         </div>
     )
 }
