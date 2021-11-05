860728e373f131b2679437211053cb3b490f50e0
Nick <nickmagi24@gmail.com>
2021-11-05 13:42:00 -0400

    Menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,11 +19,47 @@ function Navbar() {
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+
                     <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        <Link 
+                            to='/' 
+                            className='nav-links' 
+                            onClick={closeMobileMenu}
+                            >
                             Home
                         </Link>
                     </li>
+
+                    <li className='nav-item'>
+                        <Link 
+                            to='/services' 
+                            className='nav-links' 
+                            onClick={closeMobileMenu}
+                            >
+                            Services
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link 
+                            to='/products' 
+                            className='nav-links' 
+                            onClick={closeMobileMenu}
+                            >
+                            Portfolio
+                        </Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        <Link 
+                            to='/contact' 
+                            className='nav-links-mobile' 
+                            onClick={closeMobileMenu}
+                            >
+                            Contact
+                        </Link>
+                    </li>
+
                 </ul>
             </div>
         </nav>           
